ef652b80e01ec2a1329bf38c763307d419f5d1fe
ivanZanardi <zanardi3@illinois.edu>
2024-11-23 10:09:02 -0600

    misc

--- examples/RVC_N3/inputs/max_mom_2/allrun.sh
+++ examples/RVC_N3/inputs/max_mom_2/allrun.sh
@@ -2,8 +2,8 @@
 
 path_to_scripts=/home/zanardi/Codes/ML/RONEK/ronek/ronek/scripts/
 
-echo -e "\nRunning 'build_rom' script ..."
-python -u $path_to_scripts/build_rom.py --inpfile build_rom.json
+# echo -e "\nRunning 'build_rom' script ..."
+# python -u $path_to_scripts/build_rom.py --inpfile build_rom.json
 
 echo -e "\nRunning 'eval_rom_acc' script ..."
 python -u $path_to_scripts/eval_rom_acc.py --inpfile eval_rom_acc.json
