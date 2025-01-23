8a916f8f71dde6bbda1fcdd1ed49af6757d57911
ivanZanardi <zanardi3@illinois.edu>
2025-01-23 15:11:53 -0600

    misc

--- ronek2/systems/argoncr_molar.py
+++ ronek2/systems/argoncr_molar.py
@@ -57,7 +57,6 @@ class ArgonCR(object):
       kinetics=self.kin,
       radiation=None
     )
-    self.isothermal = bool(isothermal)
     # ROM
     # -------------
     # Bases
@@ -72,73 +71,76 @@ class ArgonCR(object):
     self.output_lin = True
     self.C = None
 
-  # # Equilibrium composition
-  # # ===================================
-  # def compute_eq_comp(self, p, T):
-  #   # Solve this system of equations:
-  #   # -------------
-  #   # 1) Charge neutrality:
-  #   #    x_em = x_Arp
-  #   # 2) Mole conservation:
-  #   #    x_em + x_Arp + x_Ar = 1
-  #   # 3) Detailed balance:
-  #   #    (n_Arp*n_em)/n_Ar = (Q_Arp*Q_em)/Q_Ar
-  #   # -------------
-  #   # Update thermo
-  #   self.mix.update_species_thermo(T)
-  #   # Compute number density
-  #   n = p / (const.UKB*T)
-  #   # Compute coefficient for quadratic system
-  #   f = self.mix.species["Arp"].Q * self.mix.species["em"].Q
-  #   f /= (self.mix.species["Ar"].Q * n)
-  #   # Solve quadratic system for 'x_em'
-  #   a = 1.0
-  #   b = 2.0 * f
-  #   c = -f
-  #   x = (-b+torch.sqrt(b**2-4*a*c))/(2*a)
-  #   x = torch.clip(x, const.XMIN, 1.0)
-  #   # Set molar fractions
-  #   s = self.mix.species["em"]
-  #   s.x = x
-  #   s = self.mix.species["Arp"]
-  #   s.x = x * s.q / s.Q
-  #   s = self.mix.species["Ar"]
-  #   s.x = (1.0-2.0*x) * s.q / s.Q
-  #   # Number densities
-  #   n = n * torch.cat([self.mix.species[k].x for k in self.species_order])
-  #   # Density
-  #   rho = self.mix.get_rho(n)
-  #   return n, rho
 
-  # # Initial composition
-  # # ===================================
-  # def get_init_composition(self, p, T, noise=False, sigma=1e-2):
-  #   # Compute equilibrium mass fractions
-  #   n, rho = self.compute_eq_comp(p, T)
-  #   # Add random noise
-  #   if noise:
-  #     # > Electron
-  #     s = self.mix.species["em"]
-  #     x = s.x * self._add_norm_noise(s, sigma, use_q=False)
-  #     x = torch.clip(x, const.XMIN, 1.0)
-  #     s.x = x
-  #     # > Argon Ion
-  #     s = self.mix.species["Arp"]
-  #     s.x = x * self._add_norm_noise(s, sigma)
-  #     # > Argon
-  #     s = self.mix.species["Ar"]
-  #     s.x = (1.0-2.0*x) * self._add_norm_noise(s, sigma)
-  #     # Number densities
-  #     n = torch.sum(n)
-  #     n = n * torch.cat([self.mix.species[k].x for k in self.species_order])
-  #   return n, rho
+    self.isothermal = bool(isothermal)
 
-  # def _add_norm_noise(self, species, sigma, use_q=True):
-  #   f = 1.0 + sigma * torch.rand(species.nb_comp)
-  #   if use_q:
-  #     f *= species.q * f
-  #     f /= torch.sum(f)
-  #   return f
+  # Equilibrium composition
+  # ===================================
+  def compute_eq_comp(self, p, T):
+    # Solve this system of equations:
+    # -------------
+    # 1) Charge neutrality:
+    #    x_em = x_Arp
+    # 2) Mole conservation:
+    #    x_em + x_Arp + x_Ar = 1
+    # 3) Detailed balance:
+    #    (n_Arp*n_em)/n_Ar = (Q_Arp*Q_em)/Q_Ar
+    # -------------
+    # Update thermo
+    self.mix.update_species_thermo(T)
+    # Compute number density
+    n = p / (const.UKB*T)
+    # Compute coefficient for quadratic system
+    f = self.mix.species["Arp"].Q * self.mix.species["em"].Q
+    f /= (self.mix.species["Ar"].Q * n)
+    # Solve quadratic system for 'x_em'
+    a = 1.0
+    b = 2.0 * f
+    c = -f
+    x = (-b+torch.sqrt(b**2-4*a*c))/(2*a)
+    x = torch.clip(x, const.XMIN, 1.0)
+    # Set molar fractions
+    s = self.mix.species["em"]
+    s.x = x
+    s = self.mix.species["Arp"]
+    s.x = x * s.q / s.Q
+    s = self.mix.species["Ar"]
+    s.x = (1.0-2.0*x) * s.q / s.Q
+    # Number densities
+    n = n * torch.cat([self.mix.species[k].x for k in self.species_order])
+    # # Density
+    # rho = self.mix.get_rho(n)
+    return n#, rho
+
+  # Initial composition
+  # ===================================
+  def get_init_composition(self, p, T, noise=False, sigma=1e-2):
+    # Compute equilibrium mass fractions
+    n, rho = self.compute_eq_comp(p, T)
+    # Add random noise
+    if noise:
+      # > Electron
+      s = self.mix.species["em"]
+      x = s.x * self._add_norm_noise(s, sigma, use_q=False)
+      x = torch.clip(x, const.XMIN, 1.0)
+      s.x = x
+      # > Argon Ion
+      s = self.mix.species["Arp"]
+      s.x = x * self._add_norm_noise(s, sigma)
+      # > Argon
+      s = self.mix.species["Ar"]
+      s.x = (1.0-2.0*x) * self._add_norm_noise(s, sigma)
+      # Number densities
+      n = torch.sum(n)
+      n = n * torch.cat([self.mix.species[k].x for k in self.species_order])
+    return n, rho
+
+  def _add_norm_noise(self, species, sigma, use_q=True):
+    f = 1.0 + sigma * torch.rand(species.nb_comp)
+    if use_q:
+      f *= species.q * f
+      f /= torch.sum(f)
+    return f
 
   # ROM Basis
   # ===================================
@@ -180,7 +182,7 @@ class ArgonCR(object):
     # > Conservative variables
     f_rho, f_et, f_ee = self.sources(n, T, Te)
     # > Primitive variables
-    f_w = self.mix.ov_rho * f_rho
+    f_n = self.mix.m_inv_mat @ f_rho
     if self.isothermal:
       f_e = torch.zeros(2)
     else:
@@ -189,7 +191,7 @@ class ArgonCR(object):
         self.omega_pe(f_ee)]
       )
     # > Concatenate
-    f = torch.cat([f_w, f_e])
+    f = torch.cat([f_n/const.UNA, f_e])
     # ROM activated
     if self.use_rom:
       f = self._encode(f)
@@ -197,9 +199,9 @@ class ArgonCR(object):
 
   def get_prim(self, y):
     # Unpacking
-    w, T, Tpe = y[:-2], y[-2], y[-1]
+    c, T, Tpe = y[:-2], y[-2], y[-1]
     # Get number densities
-    n = self.mix.get_n(w)
+    n = const.UNA * c
     # Get electron temperature
     Te = Tpe if self.isothermal else self.mix.get_Te(pe=Tpe, ne=n[-1])
     # Clip temperatures
