d127f5d16f659c2e446bbf0de45ba4ed33ac0a60
ivanZanardi <zanardi3@illinois.edu>
2025-04-14 14:54:11 -0500

    misc

--- examples/RVC_O3/inputs/gen_data/allrun.sh
+++ examples/RVC_O3/inputs/gen_data/allrun.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -i
 
-path_to_scripts=/home/zanardi/Codes/ML/RONEK/ronek/ronek/scripts/
+path_to_scripts=/home/zanardi/Codes/ML/RONEK/ronek/scripts/
 
+echo -e "\nRunning 'gen_data' script ..."
 python -u $path_to_scripts/gen_data.py --inpfile gen_data.json
